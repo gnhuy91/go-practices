a65e8448c638a8f1b735d58a6d1643fbe9daa431
Huy Giang <gnhuy91@gmail.com>
2016-05-19 18:02:09 +0700

    formated with goimports

--- read_file.go
+++ read_file.go
@@ -7,61 +7,61 @@
 package main
 
 import (
-    "fmt"
-    "io"
-    "bufio"
-    "os"
-    "log"
-    "sort"
+	"bufio"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"sort"
 )
 
 func main() {
-    filename := "text.txt"
-    _, strLines, err := readFileLine(filename)
-    if err != nil {
-        panic(err)
-    }
+	filename := "text.txt"
+	_, strLines, err := readFileLine(filename)
+	if err != nil {
+		panic(err)
+	}
 
-    // remove blank lines from the slice
-    for i, line := range strLines {
-        if line == "\n" {
-            strLines = append(strLines[:i], strLines[i+1:]...)
-        }
-    }
-    // sort the slice by line
-    sort.Strings(strLines)
-    fmt.Println(strLines)
+	// remove blank lines from the slice
+	for i, line := range strLines {
+		if line == "\n" {
+			strLines = append(strLines[:i], strLines[i+1:]...)
+		}
+	}
+	// sort the slice by line
+	sort.Strings(strLines)
+	fmt.Println(strLines)
 }
 
 func readFileLine(filename string) ([]byte, []string, error) {
-    var (
-        byteLines []byte
-        strLines []string
-    )
+	var (
+		byteLines []byte
+		strLines  []string
+	)
 
-    // open the file
-    o, err := os.Open(filename)
-    if err != nil {
-        return nil, nil, err
-    }
-    defer o.Close()
+	// open the file
+	o, err := os.Open(filename)
+	if err != nil {
+		return nil, nil, err
+	}
+	defer o.Close()
 
-    // start reading the file line by line
-    r := bufio.NewReader(o)
-    for {
-        // ReadLine reads the line excluding newline characters
-        l, _, err := r.ReadLine()
-        if err != nil {
-            if err == io.EOF {
-                break
-            }
-            log.Println(err)
-        }
-        byteLines = append(byteLines, l...)
-        // also append newline byte back so that result slice will include
-        // newline characters like the original file
-        byteLines = append(byteLines, []byte("\n")...)
-        strLines = append(strLines, string(l) + "\n")
-    }
-    return byteLines, strLines, nil
+	// start reading the file line by line
+	r := bufio.NewReader(o)
+	for {
+		// ReadLine reads the line excluding newline characters
+		l, _, err := r.ReadLine()
+		if err != nil {
+			if err == io.EOF {
+				break
+			}
+			log.Println(err)
+		}
+		byteLines = append(byteLines, l...)
+		// also append newline byte back so that result slice will include
+		// newline characters like the original file
+		byteLines = append(byteLines, []byte("\n")...)
+		strLines = append(strLines, string(l)+"\n")
+	}
+	return byteLines, strLines, nil
 }
