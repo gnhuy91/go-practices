1f7d4aa06af70d651d17d953704d2bfa8a1f9abd
Huy Giang <gnhuy91@gmail.com>
2016-05-25 23:45:59 +0700

    added http api example

---
+++ http.go
@@ -0,0 +1,38 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+)
+
+type Message struct {
+	Text string `json:"msg"`
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	s := fmt.Sprintf("Welcome, %s!", r.URL.Path[1:])
+	// if composed the string in a struct, json.Marshal can return json (key&value)
+	// based on struct field name (if no `json:field` specified) and string content.
+	j, _ := json.Marshal(&Message{s})
+	w.Write(j)
+}
+
+func about(w http.ResponseWriter, r *http.Request) {
+	m := Message{"go API, build v0.0.001.992."}
+	j, _ := json.Marshal(&m)
+	w.Write(j)
+}
+
+func hello(w http.ResponseWriter, r *http.Request) {
+	// If a raw value, Marshal doesn't seem to do anything.
+	j, _ := json.Marshal("Hello from API")
+	w.Write(j)
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/about", about)
+	http.HandleFunc("/hello", hello)
+	http.ListenAndServe(":8080", nil)
+}
