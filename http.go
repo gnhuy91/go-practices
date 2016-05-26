3199598faea5e459d2374b803b7a03087f099c3e
Huy Giang <gnhuy91@gmail.com>
2016-05-26 22:13:58 +0700

    added Header modification for httpResponse

--- http.go
+++ http.go
@@ -19,6 +19,8 @@ func handler(w http.ResponseWriter, r *http.Request) {
 }
 
 func about(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(http.StatusOK)
 	m := Message{"go API, build v0.0.001.992."}
 	j, _ := json.Marshal(&m)
 	w.Write(j)
