6d0940db82fc2a0001d90de74396d9bf03900cd0
Huy Giang <gnhuy91@gmail.com>
2016-06-02 13:30:20 +0700

    added channels example

---
+++ chan.go
@@ -0,0 +1,32 @@
+// https://gobyexample.com/select
+
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	c1 := make(chan string)
+	c2 := make(chan string)
+
+	go func() {
+		time.Sleep(time.Second * 2)
+		c1 <- "one"
+	}()
+
+	go func() {
+		time.Sleep(time.Second * 2)
+		c2 <- "two"
+	}()
+
+	for range []chan string{c1, c2} {
+		select {
+		case msg1 := <-c1:
+			fmt.Println("received", msg1)
+		case msg2 := <-c2:
+			fmt.Println("received", msg2)
+		}
+	}
+}
