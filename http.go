fbd07ede920d1a880744bc5a526974d8d0bd8e67
Huy Giang <gnhuy91@gmail.com>
2016-06-09 21:29:58 +0700

    added examples with gorilla/mux

--- http.go
+++ http.go
@@ -5,8 +5,13 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	_ "net/http/pprof"
+	"os"
 	"time"
+
+	"github.com/gorilla/mux"
 )
 
 type Message struct {
@@ -14,7 +19,7 @@ type Message struct {
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-	s := fmt.Sprintf("Welcome, %s!", r.URL.Path[1:])
+	s := fmt.Sprintf("Welcome!")
 	// if composed the string in a struct, json.Marshal can return json (key&value)
 	// based on struct field name (if no `json:field` specified) and string content.
 	j, _ := json.Marshal(&Message{s})
@@ -41,10 +46,35 @@ func serve(w http.ResponseWriter, r *http.Request) {
 	// test: for i in $(seq 1 3); do (time curl -sS localhost:8080/serve &); done
 }
 
+// took from the awesome: https://gist.github.com/tsenart/5fc18c659814c078378d
+func userHandler() http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		vars := mux.Vars(r)
+		name := vars["name"]
+		fmt.Fprintf(w, "user: %s", name)
+	})
+}
+
+func withMetrics(l *log.Logger, next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		began := time.Now()
+		next.ServeHTTP(w, r)
+		l.Printf("%s: %s %s took %s", time.Now(), r.Method, r.URL, time.Since(began))
+	})
+}
+
 func main() {
-	http.HandleFunc("/", handler)
-	http.HandleFunc("/about", about)
-	http.HandleFunc("/hello", hello)
-	http.HandleFunc("/serve", serve)
-	http.ListenAndServe(":8080", nil)
+	// Create our logger
+	logger := log.New(os.Stdout, "", 0)
+
+	r := mux.NewRouter()
+	r.HandleFunc("/", handler)
+	r.HandleFunc("/about", about)
+	r.HandleFunc("/hello", hello)
+	r.HandleFunc("/serve", serve)
+
+	r.Handle("/user/{name}", userHandler())
+	r.Handle("/_user/{name}", withMetrics(logger, userHandler()))
+
+	http.ListenAndServe(":8080", r)
 }
